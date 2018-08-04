2cc160a8036271c56d6595660fb3533a9359f5f7
Md. Sazid Hossain <sazid140233@gmail.com>
2018-08-04 18:22:46 +0600

    asdfasdf

--- qb/resources/views/superadmin/questions/create.blade.php
+++ qb/resources/views/superadmin/questions/create.blade.php
@@ -16,7 +16,14 @@ $(document).ready(function(){
 
     $('#company').on('change', function(event){
         company_id = event.target.value;
-        
+        $('#program').empty();
+        $('#program').append('<option>--Select Program--</option>');
+        $('#subject').empty();
+        $('#subject').append('<option>--Select Subject--</option>');
+        $('#chapter').empty();
+        $('#chapter').append('<option>--Select Chapter--</option>');
+        $('#topic').empty();
+        $('#topic').append('<option>--Select Topic--</option>');
         var url = '/programs/getProgramAccordingToCompany';
         $.ajax({
             url:url,
@@ -44,7 +51,12 @@ $(document).ready(function(){
     $('#program').on('change', function(event){
         
         program_id = event.target.value;
-        
+        $('#subject').empty();
+        $('#subject').append('<option>--Select Subject--</option>');
+        $('#chapter').empty();
+        $('#chapter').append('<option>--Select Chapter--</option>');
+        $('#topic').empty();
+        $('#topic').append('<option>--Select Topic--</option>');
         var url = '/subjects/getSubjectAccordingToProgram';
         $.ajax({
             url:url,
@@ -55,7 +67,9 @@ $(document).ready(function(){
             },
             dataType: 'json',
             success:function(data)
-            {             
+            {
+                
+                            
                 $.each(data,function(index,subjectsObjectForSelectedProgram){
                     
                     subjectsObjectForSelectedProgram.forEach(function(element){
@@ -73,7 +87,10 @@ $(document).ready(function(){
     $('#subject').on('change', function(event){
         
         subject_id = event.target.value;
-        
+        $('#chapter').empty();
+        $('#chapter').append('<option>--Select Chapter--</option>');
+        $('#topic').empty();
+        $('#topic').append('<option>--Select Topic--</option>');
         var url = '/chapters/getChapterAccordingToSubject';
         $.ajax({
             url:url,
@@ -100,7 +117,8 @@ $(document).ready(function(){
     $('#chapter').on('change', function(event){
         
         chapter_id = event.target.value;
-        
+        $('#topic').empty();
+        $('#topic').append('<option>--Select Topic--</option>');
         var url = '/topics/getTopicAccordingToChapter';
         $.ajax({
             url:url,
@@ -124,11 +142,37 @@ $(document).ready(function(){
         })
     });
 
+    $('#topic').on('change', function(){
+        topic_id = event.target.value;
+        var question = 
+    });
+
+    $('#submit').on('click', function(){
+        var question = document.getElementById("mcq_question").value;
+        var url = '/questions/store';
+        
+        $.ajax({
+            url:url,
+            method:'POST',
+            data:{
+                topic_id:topic_id,
+                question:question,
+                option1:option1,
+                option2:option2,
+                option3:option3,
+                option4:option4,
+                
+                
+            },
+        })
+    });
+
 });
 
 </script>
 
 <div>
+    {{ csrf_field() }}
     <div class="row">
         <div class="col-md-4">
             <div id="choose_company">
@@ -188,27 +232,61 @@ $(document).ready(function(){
 <br>
 <div id="mcq-question-section">
 
-<div class="row">
-    <div class="col-md-6">
+    <div class="row">
+        <div class="col-md-6">
 
-        <div id="question">
-            <h4><label>Enter Your Question Here</label></h4>
-            <textarea rows="9" class="form-control" id="mcq_question"></textarea>
+            <div id="question">
+                <h4><label>Enter Your Question Here</label></h4>
+                <textarea rows="9" class="form-control" id="mcq_question"></textarea>
+            </div>
+        </div>
+        
+        <div class="col-md-6">
+            <div id="options">
+                <h4><label>Enter Your Options Here</label></h4>
+                <input type="text" class="form-control" placeholder="Option 1" id="mcq_option_1">
+                <br>
+                <input type="text" class="form-control" placeholder="Option 2" id="mcq_option_2">
+                <br>
+                <input type="text" class="form-control" placeholder="Option 3" id="mcq_option_3">
+                <br>
+                <input type="text" class="form-control" placeholder="Option 4" id="mcq_option_4">
+            </div>
         </div>
     </div>
-    
-    <div class="col-md-6">
-        <div id="options">
-            <h4><label>Enter Your Options Here</label></h4>
-            <input type="text" class="form-control" placeholder="Option 1" id="mcq_option_1">
-            <br>
-            <input type="text" class="form-control" placeholder="Option 2" id="mcq_option_2">
-            <br>
-            <input type="text" class="form-control" placeholder="Option 3" id="mcq_option_3">
-            <br>
-            <input type="text" class="form-control" placeholder="Option 4" id="mcq_option_4">
+    <br>
+    <div class="row">
+        <div class="col-md-6">
+            <div id="choose-priority">
+                <h4><label>Select Priority</label></h4>
+                <select class="form-control" name="priority" data-style="select-with-transition" title="Select Topic" id="priority" >
+                    <option>--Select Priority--</option>
+                    <option value="1">1 - Lowest</option>
+                    <option value="2">2 - Lower</option>
+                    <option value="3">3 - Medium</option>
+                    <option value="4">4 - Higher</option>
+                    <option value="5">5 - Highest</option>
+                </select>
+            </div>
+        </div>
+
+        <div class="col-md-6">
+            <div id="choose-priority">
+                <h4><label>Select Difficulty</label></h4>
+                <select class="form-control" name="difficulty" data-style="select-with-transition" title="Select Topic" id="difficulty" >
+                    <option>--Select Difficulty--</option>
+                    <option value="1">Easy</option>
+                    <option value="2">Medium</option>
+                    <option value="3">Hard</option>
+                </select>
+            </div>
         </div>
     </div>
+    <br>
+<br>
+<div>
+    <button type="submit" class="btn btn-med btn-primary">Add Question</button>
 </div>
+<br>
 
 @endsection
